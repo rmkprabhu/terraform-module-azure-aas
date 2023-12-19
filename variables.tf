11ff55e43c949271a692e9644205493b87f270ae
rmkprabhu <manikandaprabhuravi@gmail.com>
2023-12-19 16:14:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "location" {
+    type = string
+}
+variable "rg_name" {
+    type = string
+}
+variable "rg_name_01" {
+    type = string
+}
+variable "vnet_name" {
+    type = string
+}
+variable "subnet_name" {
+    type = string
+}
+variable "vm_name" {
+    type = string
+}
+
+variable "TF_LOG" {
+    type = string
+    
+}
