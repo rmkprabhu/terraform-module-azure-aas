ee3e729acd7f73c1a67bbc4262ddf8d0dfde7591
rmkprabhu <manikandaprabhuravi@gmail.com>
2023-12-19 16:13:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,9 @@ data "azurerm_virtual_machine" "myvm" {
   resource_group_name  = var.rg_name_01
 }
 
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "${var.vm_name}-public-ip-01"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Dynamic"
+}
